4e36d111b640f3f20a8e0dfccf0e23ea714d1282
skotaki5 <98348704+skotaki5@users.noreply.github.com>
2022-05-25 01:14:40 +0530

    Update create database.py
    Resolved conflicts while merging dev2 into dev.

--- SAIL_main/Development/SAIL/Databricks/SAIL/init_scripts/create database.py
+++ SAIL_main/Development/SAIL/Databricks/SAIL/init_scripts/create database.py
@@ -5,16 +5,4 @@
 # COMMAND ----------
 
 # MAGIC %sql
-<<<<<<< HEAD
 # MAGIC create database gold
-=======
-# MAGIC create database gold
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC show create table silver.dim_customer;
-
-# COMMAND ----------
-
->>>>>>> c38a47b (Importing Dev2 code to dev2 branch)
